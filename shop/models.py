a06d80f2f3717d1f104c25ec99ee90cb502a1b3a
OnurDemirci <onuristik@gmail.com>
2022-05-23 20:47:23 +0300

    - Deleted management app, was causing code repetition. - Created models. - Created index URL and added shop app's urls to main project. - Created index view and index template (empty for now)

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+
+class FavoriteShop(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class FavoriteProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+
+
+class IndexView(generic.ListView):
+    template_name = 'shop/index.html'
+    context_object_name = 'latest_products'
